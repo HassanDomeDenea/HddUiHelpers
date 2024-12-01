781d1759dedca646a13b3ff62d49f1f8ec6b55e3
HassanDomeDenea <hassan.domedenea@gmail.com>
2024-12-01 11:50:02 +0300

    initial commit

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MyComponent',
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
