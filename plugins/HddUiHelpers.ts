c22101e9cbb5ac2129549f22937ef805e611c4d8
HassanDomeDenea <hassan.domedenea@gmail.com>
2025-12-11 01:11:33 +0300

    making onlyRequestedColumns default to false

--- plugins/HddUiHelpers.ts
+++ plugins/HddUiHelpers.ts
@@ -1,55 +1,70 @@
-import type { AxiosInstance } from 'axios';
-import type { ServerDataTableProps } from 'HddUiHelpers/components/datatables/ServerDataTableTypes.ts';
-import { i18n } from 'HddUiHelpers/plugins/i18n';
-import pinia from 'HddUiHelpers/plugins/pinia';
-import primevue from 'HddUiHelpers/plugins/primevue';
-import { useApiClient } from 'HddUiHelpers/stores/apiClient';
-import primeVueLocales from 'HddUiHelpers/utils/primeVueLocales.ts';
-import type { App, InjectionKey } from 'vue';
-import { inject } from 'vue';
-import type { I18n } from 'vue-i18n';
-import YAML from 'yaml';
+import type { AxiosInstance } from 'axios'
+import type { ServerDataTableProps } from 'HddUiHelpers/components/datatables/ServerDataTableTypes.ts'
+import { i18n } from 'HddUiHelpers/plugins/i18n'
+import pinia from 'HddUiHelpers/plugins/pinia'
+import primevue from 'HddUiHelpers/plugins/primevue'
+import { useApiClient } from 'HddUiHelpers/stores/apiClient'
+import primeVueLocales from 'HddUiHelpers/utils/primeVueLocales.ts'
+import type { App, InjectionKey } from 'vue'
+import { inject } from 'vue'
+import type { I18n } from 'vue-i18n'
+import YAML from 'yaml'
 
 export interface HddUiHelpers {
-  axiosInstance: AxiosInstance;
-  routeNameResolver: (name: string, parameter?: string | number) => string;
-  i18nInstance: I18n<any>;
-  commonServerDataTableProps: Partial<ServerDataTableProps>;
-  withBroadcasting: boolean;
-  presenceUsersChannel: string | null;
-  dateArabicMonthNamesAsNumbers?: boolean;
-  dateEnglishMonthNamesAsNumbers?: boolean;
+  axiosInstance: AxiosInstance
+  routeNameResolver: (name: string, parameter?: string | number) => string
+  i18nInstance: I18n<any>
+  onlyRequestedColumns: false
+  commonServerDataTableProps: Partial<ServerDataTableProps>
+  withBroadcasting: boolean
+  presenceUsersChannel: string | null
+  dateArabicMonthNamesAsNumbers?: boolean
+  dateEnglishMonthNamesAsNumbers?: boolean
 }
 
 export interface HddUiHelpersComposable {
-  axiosInstance: AxiosInstance;
-  route: (name: string, parameter?: string | number) => string;
-  commonServerDataTableProps: Partial<ServerDataTableProps>;
-  withBroadcasting: boolean;
-  presenceUsersChannel: string | null;
+  axiosInstance: AxiosInstance
+  route: (name: string, parameter?: string | number) => string
+  commonServerDataTableProps: Partial<ServerDataTableProps>
+  withBroadcasting: boolean
+  presenceUsersChannel: string | null
 }
 
-export const HddUiHelpersSymbol: InjectionKey<Partial<HddUiHelpersComposable>> = Symbol('HddUiHelpersSymbol');
+export const HddUiHelpersSymbol: InjectionKey<Partial<HddUiHelpersComposable>> =
+  Symbol('HddUiHelpersSymbol')
 
-export type HddUiHelpersPluginOptions = Partial<HddUiHelpers>;
+export type HddUiHelpersPluginOptions = Partial<HddUiHelpers>
 
 export default {
   install(app: App, options: HddUiHelpersPluginOptions = {}) {
     const routeNameResolver =
       options.routeNameResolver ??
       function (name: string, parameter?: string | number) {
-        return `/${name}${parameter !== undefined ? `/${parameter}` : ''}`;
-      };
+        return `/${name}${parameter !== undefined ? `/${parameter}` : ''}`
+      }
     const initiallyProvidedValues = {
       route: routeNameResolver,
       commonServerDataTableProps: options.commonServerDataTableProps ?? {},
       withBroadcasting: options.withBroadcasting ?? false,
       presenceUsersChannel: options.presenceUsersChannel ?? null,
-    };
-    app.provide(HddUiHelpersSymbol, initiallyProvidedValues);
+    }
+    app.provide(HddUiHelpersSymbol, initiallyProvidedValues)
 
     if (options.dateArabicMonthNamesAsNumbers === true) {
-      primeVueLocales.ar.monthNames = ['شهر 1', 'شهر 2', 'شهر 3', 'شهر 4', 'شهر 5', 'شهر 6', 'شهر 7', 'شهر 8', 'شهر 9', 'شهر 10', 'شهر 11', 'شهر 12'];
+      primeVueLocales.ar.monthNames = [
+        'شهر 1',
+        'شهر 2',
+        'شهر 3',
+        'شهر 4',
+        'شهر 5',
+        'شهر 6',
+        'شهر 7',
+        'شهر 8',
+        'شهر 9',
+        'شهر 10',
+        'شهر 11',
+        'شهر 12',
+      ]
       primeVueLocales.ar.monthNamesShort = [
         'شهر 1',
         'شهر 2',
@@ -63,7 +78,7 @@ export default {
         'شهر 10',
         'شهر 11',
         'شهر 12',
-      ];
+      ]
     }
     if (options.dateEnglishMonthNamesAsNumbers === true) {
       primeVueLocales.en.monthNames = [
@@ -79,7 +94,7 @@ export default {
         'Month 10',
         'Month 11',
         'Month 12',
-      ];
+      ]
       primeVueLocales.en.monthNamesShort = [
         'Month 1',
         'Month 2',
@@ -93,32 +108,36 @@ export default {
         'Month 10',
         'Month 11',
         'Month 12',
-      ];
+      ]
     }
 
-    app.use(i18n);
-    app.use(pinia);
-    const apiClient = useApiClient();
+    app.use(i18n)
+    app.use(pinia)
+    const apiClient = useApiClient()
     if (options.axiosInstance) {
-      apiClient.setAxiosInstance(options.axiosInstance);
+      apiClient.setAxiosInstance(options.axiosInstance)
     }
 
-    app.use(primevue);
+    app.use(primevue)
     app.provide(HddUiHelpersSymbol, {
       ...initiallyProvidedValues,
       axiosInstance: apiClient.axiosInstance,
-    });
+    })
 
     // Extend the locales
-    const locales = import.meta.glob('../locales/*.yaml', { eager: true, query: '?raw', import: 'default' });
+    const locales = import.meta.glob('../locales/*.yaml', {
+      eager: true,
+      query: '?raw',
+      import: 'default',
+    })
     for (const [key, value] of Object.entries(locales)) {
-      const locale = key.split('/').pop()?.split('.')[0]; // Extract locale name (e.g., "en", "fr")
+      const locale = key.split('/').pop()?.split('.')[0] // Extract locale name (e.g., "en", "fr")
       if (locale) {
-        i18n.global.mergeLocaleMessage(locale, YAML.parse(value as string));
+        i18n.global.mergeLocaleMessage(locale, YAML.parse(value as string))
       }
     }
   },
-};
+}
 
 /*
 export default function HddUiHelpersPlugin(options: HddUiHelpersPluginOptions = {}): Plugin {
@@ -129,15 +148,15 @@ export default function HddUiHelpersPlugin(options: HddUiHelpersPluginOptions = 
 */
 
 export function useHddUiHelpers(): HddUiHelpersComposable {
-  const apiClient = useApiClient();
+  const apiClient = useApiClient()
   return {
     axiosInstance: apiClient.axiosInstance,
     route(name: string, parameter?: string | number) {
-      return `/${name}${parameter !== undefined ? `/${parameter}` : ''}`;
+      return `/${name}${parameter !== undefined ? `/${parameter}` : ''}`
     },
     commonServerDataTableProps: {},
     withBroadcasting: false,
     presenceUsersChannel: null,
     ...inject(HddUiHelpersSymbol),
-  };
+  }
 }
